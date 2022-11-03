a38ed5cd1d0754df719c6fa902d426cf32afd166
Ванин Артем <111682303+VaninArtemOleksandrovich@users.noreply.github.com>
2022-11-03 19:15:16 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,5 +1,5 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+
 
 console.log(galleryItems);
 const galleryContainerEl = document.querySelector('.gallery'); // !
@@ -30,7 +30,7 @@ function openModal(event) {
 
 
 
-var lightbox = new SimpleLightbox('.gallery a', {
+let lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
   captionPosition: 'bottom',
   captionDelay: 250,
